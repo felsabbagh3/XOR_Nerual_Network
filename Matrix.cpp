1b78a776f2c14d40f75a0af5eb9908906fa57215
felsabbagh3 <30584044+felsabbagh3@users.noreply.github.com>
2018-05-13 13:55:12 -0400

    Updated Matrix Class

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
 
+using namespace std;
 class Matrix
 {
 
 	public:
 		Matrix(int,int);
+		float & operator()(int, int) const;
 		~Matrix();
+		Matrix operator*(Matrix &);
+		void randomize();
 		int rows;
 		int cols;
 		float ** weights;
@@ -25,6 +31,7 @@ Matrix::Matrix(int rows_, int cols_)
 			weights[curr_row][curr_col] = 0;
 		}
 	}
+	srand(time(0));
 }
 
 Matrix::~Matrix()
@@ -32,8 +39,70 @@ Matrix::~Matrix()
 	delete [] weights;
 }
 
+void Matrix::randomize()
+{
+	float * row;
+	for (int curr_row = 0; curr_row < rows; curr_row++)
+	{
+		row = weights[curr_row];
+		for (int curr_col = 0; curr_col < cols; curr_col++)
+		{
+			row[curr_col] = rand() % 10;
+		}
+	}
+}
+
+float & Matrix::operator()(int y, int x) const{
+	if (y < rows && x < cols) {
+		return weights[y][x];
+	} else {
+		return weights[0][0];
+	}
+}
+
+Matrix Matrix::operator*(Matrix & other)
+{
+	Matrix final(rows, 1);
+	if (other.cols == 1)
+	{
+		float * row;
+		float sum;
+		for (int curr_row = 0; curr_row < rows; curr_row++)
+		{
+			row = weights[curr_row];
+			sum = 0;
+			for (int curr_col = 0; curr_col < cols; curr_col++)
+			{
+				sum += row[curr_col] * other(curr_col, 0);
+			}
+			final(curr_row, 0) = sum;
+		}
+	} else {
+		cout << "THERE IS AN ERROR";
+	}
+
+	return final;
+}
+
+// Overloading the output stream operator << 
+ostream & operator<<(ostream & out, Matrix & cm) {
+
+	for (int y = 0; y < cm.rows; y++) {
+		for (int x = 0; x < cm.cols; x++) {
+			out << cm(y,x) << "\t";
+		}
+		out << endl;
+	}
+
+	return out;
+}
 
 int main()
 {
-	std::cout << "HI\n";
+	Matrix m(3,2);
+	// Matrix h(2,1);
+	// Matrix y = m * h;
+	m.randomize();
+	cout << m;
 }
+
